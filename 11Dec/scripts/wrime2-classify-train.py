634f45ed1485b111f0379601e91b24dc4a022c68
Katsuhito Sudoh <ksudoh@users.noreply.github.com>
2024-12-10 12:12:57 +0900

    Update wrime2-classify-train.py

--- 11Dec/scripts/wrime2-classify-train.py
+++ 11Dec/scripts/wrime2-classify-train.py
@@ -15,24 +15,38 @@ from argparse import ArgumentParser
 ### argparse.ArgumentParserを使う
 ### ドキュメント https://docs.python.org/ja/3.6/howto/argparse.html
 parser = ArgumentParser()
-parser.add_argument("-b", "--binary", action="store_true", help="Use binary values for Bag-of-Words")
-parser.add_argument("-m", "--model", type=str, metavar="FILE", required=True, help="Output model file")
-parser.add_argument("input", type=str, metavar="FILE", help="Input file in JSON format")
+#####
+##### 【ここで必要な処理】
+##### parser.add_argument()
+##### を使って「入力するJSONのファイル名」「出力するモデルのファイル名」を指定する
+#####
 args = parser.parse_args()
 
 ### モデルファイルが既に存在する場合はエラーとしてRuntimeError例外を送出
-if os.path.exists(args.model):
-    raise RuntimeError (f"ERROR: {args.model} already exists.")
+if os.path.exists(args._): ##### args._ には add_argument で指定した名前を入れる
+    raise RuntimeError (f"ERROR: {args._} already exists.") ##### args._ には add_argument で指定した名前を入れる
 
 ### JSON形式で記録されたファイルを読み出す（train/dev/testすべてが入っていることに注意）
-with open(args.input, 'r') as rh:
+with open(args._, 'r') as rh: ##### args._ には add_argument で指定した名前を入れる
     DATASET = json.load(rh)
 
-
+##### データを抽出する関数
 def extract_wrime2 (dataset, key='train'):
     """ JSON形式に変換された WRIME ver.2 のデータから文とラベルのリストを抽出する
-
-
+        dataset['train'] には 学習データ
+        dataset['valid'] には 検証データ
+        dataset['test'] には 評価データ
+        がそれぞれ格納されていて、その中身は辞書形式の変数のリストになっている。
+        
+        リストに格納された各変数 (data とする) には、
+        data["Sentence"] には 文（文字列）
+        data["Writer_Sentiment"] には 感情のポジティブ・ネガティブを表す数値 (2, 1, 0, -1, -2)
+        がそれぞれ格納されている。
+        
+        これらの情報を取り出して、
+        ・文を _X_str というリストに、
+        ・感情の数値を 正・ゼロ・負 の3値にして _y というリストに
+        それぞれ格納して返すこと
     """
     _X_str = []
     _y = []
@@ -40,32 +54,27 @@ def extract_wrime2 (dataset, key='train'):
     if key not in dataset:
         raise RuntimeError ("ERROR: {key} is not found in the dataset.")
     else:
-        # for data in tqdm(dataset[key]):
-        #     _X_str.append (data["Sentence"].rstrip())
-        #     if data["Writer_Sentiment"] > 0: _y.append(1)
-        #     elif data["Writer_Sentiment"] < 0: _y.append(-1)
-        #     else: _y.append(0)
-        # print (f"Dataset {key} has been extracted.", file=sys.stderr)
+        for data in tqdm(dataset[key]):
+            ##### 
+            ##### 【ここで必要な処理】
+            ##### dataという変数から文と感情の数値の情報を取り出し、_X_str と _y に格納する
+            #####
 
     return _X_str, _y
 
-
 X_train_str, y_train = extract_wrime2(DATASET)
+
+##### 分類モデルの定義、とりあえず今回はコレでやってみる
+##### CountVectorizer は文を空白で区切って単語の列とし、各単語を特徴量として抽出するもの
+##### LinearSVC は分類器の一種（線形サポートベクトル分類器）
 model_pipeline = make_pipeline(
-        # CountVectorizer( binary=args.binary, ngram_range=(1,1), min_df=2 ),
         CountVectorizer(),
         LinearSVC(random_state=42, max_iter=3000)
         )
 
 model_pipeline.fit(X_train_str, y_train)
-train_accuracy = model_pipeline.score(X_train_str, y_train)
 
-# X_dev_str, y_dev = extract_wrime2(DATASET, key='dev')
-# dev_accuracy = model_pipeline.score(X_dev_str, y_dev)
-
-# print (f"Training accuracy: {100*train_accuracy} %", file=sys.stderr)
-# print (f"Dev accuracy: {100*dev_accuracy} %", file=sys.stderr)
-
-with open(args.model, 'wb') as wh:
+### 指定したファイル名にモデルを書き出す
+with open(args._, 'wb') as wh: ##### args._ には add_argument で指定した名前を入れる
     pickle.dump(model_pipeline, wh)
-    print (f"Success! The trained model has been saved into {args.model}.", file=sys.stderr)
+    print (f"Success! The trained model has been saved into {args._}.", file=sys.stderr) ##### args._ には add_argument で指定した名前を入れる
